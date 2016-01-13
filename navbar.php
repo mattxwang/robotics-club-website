aa0fe8b275a956ca716584fcb52cda061b44e39a
malsf21 <matthewang.wang@gmail.com>
2016-01-12 20:04:15 -0500

    swaps to php, modularises navbar/footer

---
+++ navbar.php
@@ -0,0 +1,71 @@
+<!-- Navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-func">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Upper Canada College Robotics</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="navbar-collapse-func">
+			<ul class="nav navbar-nav">
+				<li><a href="index.html">Home</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">About <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Schedule</a></li>
+						<li><a href="#">Resources</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">VEX <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">About VEX</a></li>
+						<li><a href="#">Competition Schedule</a></li>
+						<li><a href="#">Competition Resources</a></li>
+						<li class="divider"></li>
+						<li><a href="http://www.vexrobotics.com/">VEX Website</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Horizons <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">About Horizons</a></li>
+						<li><a href="#">Horizons Schedule</a></li>
+						<li><a href="#">Horizons Curriculum</a></li>
+						<li class="divider"></li>
+						<li><a href="https://bluenet.ucc.on.ca/horizons">Horizons Website</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Projects <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Arduino</a></li>
+						<li><a href="#">Keyboards</a></li>
+						<li><a href="#">Soldering</a></li>
+					</ul>
+				</li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right" style="padding-right:20px;">
+				<li class="dropdown">
+					<button type="button" class="btn btn-success navbar-btn" href="#" class="dropdown-toggle" data-toggle="dropdown">
+					Welcome <b>User</b> <span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Notifications </a></li>
+						<li class="divider"></li>
+						<li><a href="#">Account Settings</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
