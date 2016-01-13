01f7f44e684819f7d48ea41680c1188b635d501c
malsf21 <matthewang.wang@gmail.com>
2016-01-13 11:10:20 -0500

    re-inserts test case

--- night_mode.php
+++ night_mode.php
@@ -1,11 +1,16 @@
 <?php 
-
+/*
 if(empty($_SESSION)$_SESSION['nightmode'] = False) {
 	$nightmode = False;
 }
 else{
 	$nightmode = True;
 }
+*/
+/*
+$nightmode = True;
+*/
+$nightmode = False;
 
 if($nightmode == True){
 	echo "<link href='css/bootstrap-night.css' rel='stylesheet'>";
