6d28fd4c059ea1222087647dbda318764645108c
malsf21 <matthewang.wang@gmail.com>
2016-01-13 00:51:33 -0500

    fixes quotations errors

--- night_mode.php
+++ night_mode.php
@@ -10,10 +10,10 @@ else{
 var nightmode = True;
 
 if(nightmode==True){
-	echo "<link href="css/bootstrap-night.css" rel="stylesheet">"
+	echo "<link href='css/bootstrap-night.css' rel='stylesheet'>"
 }
 else{
-	echo "<link href="css/bootstrap.css" rel="stylesheet">"
+	echo "<link href='css/bootstrap.css' rel='stylesheet'>"
 }
 
 ?> 
