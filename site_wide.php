e1499b2931ed1227d810b585d90b37e4a683e7d6
malsf21 <matthewang.wang@gmail.com>
2016-01-13 00:33:48 -0500

    trying out alert transitions

--- site_wide.php
+++ site_wide.php
@@ -1,4 +1,4 @@
-<div class="alert alert-dismissible alert-warning">
+<div class="alert alert-dismissible alert-warning fade in">
 	<button type="button" class="close" data-dismiss="alert">x</button>
 	<h4>Warning!</h4>
 	<p>You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a>.</p>
