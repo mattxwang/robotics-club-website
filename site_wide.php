ff3836acd27acf91e8da0751e1060dbdb5659f4b
malsf21 <matthewang.wang@gmail.com>
2016-01-13 20:47:21 -0500

    uses alert link class

--- site_wide.php
+++ site_wide.php
@@ -7,5 +7,5 @@
 <div class="alert alert-dismissible alert-info fade in">
 	<button type="button" class="close" data-dismiss="alert">x</button>
 	<h4>Hey there!</h4>
-	<p>We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca">Matthew Wang</a> for more information.</p>
+	<p>We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
 </div>
