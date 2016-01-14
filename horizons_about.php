62d480b791480399881299811bf598167d351a50
malsf21 <matthewang.wang@gmail.com>
2016-01-14 15:30:38 -0500

    fixes about styling

--- horizons_about.php
+++ horizons_about.php
@@ -19,11 +19,11 @@
 			<div class="jumbotron bg-success">
 				<h3>Horizons Robotics</h3>
 				<blockquote><h1>Every child deserves the right to an education.</h1></blockquote>
-				<h4 style="text-align:right;">- Barack Obama, President of the United States</h4>
+				<h4 style="text-align:right;">- Barack Obama, 44th President of the United States</h4>
 			</div>
 			<div class="row">
 				<div class="col-lg-6">
-					<div class="panel panel-danger">
+					<div class="panel panel-success">
 						<div class="panel-heading">
 							<h3 class="panel-title">What's Horizons?</h3>
 						</div>
@@ -31,7 +31,7 @@
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices, sapien sit amet vestibulum tincidunt, sapien arcu laoreet diam, vel convallis felis tellus a est. Nunc in sagittis quam. Aliquam ac ipsum eu velit tempor egestas eget vel leo. Aliquam erat volutpat. Suspendisse lacinia dui at libero tincidunt accumsan. Morbi laoreet dui quam, eu volutpat sapien condimentum ut. Nam cursus aliquet dolor varius cursus. In ut magna augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vehicula est quam, ac luctus dui iaculis sit amet. Sed tincidunt elementum dui, at molestie turpis feugiat ac. Etiam quis nisi sit amet eros placerat interdum eu vitae tortor. Duis sollicitudin nisi id purus bibendum, sed tempor arcu tempus. Morbi pellentesque fermentum lectus et vehicula.
 						</div>
 					</div>
-					<div class="panel panel-danger">
+					<div class="panel panel-success">
 						<div class="panel-heading">
 							<h3 class="panel-title">What's Horizons Robotics?</h3>
 						</div>
@@ -39,7 +39,7 @@
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices, sapien sit amet vestibulum tincidunt, sapien arcu laoreet diam, vel convallis felis tellus a est. Nunc in sagittis quam. Aliquam ac ipsum eu velit tempor egestas eget vel leo. Aliquam erat volutpat. Suspendisse lacinia dui at libero tincidunt accumsan. Morbi laoreet dui quam, eu volutpat sapien condimentum ut. Nam cursus aliquet dolor varius cursus. In ut magna augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vehicula est quam, ac luctus dui iaculis sit amet. Sed tincidunt elementum dui, at molestie turpis feugiat ac. Etiam quis nisi sit amet eros placerat interdum eu vitae tortor. Duis sollicitudin nisi id purus bibendum, sed tempor arcu tempus. Morbi pellentesque fermentum lectus et vehicula.
 						</div>
 					</div>
-					<div class="panel panel-danger">
+					<div class="panel panel-success">
 						<div class="panel-heading">
 							<h3 class="panel-title">How do I participate?</h3>
 						</div>
