f829ae9fd971cd2bc451889aa1b2c02de43326b3
malsf21 <matthewang.wang@gmail.com>
2016-01-13 20:37:03 -0500

    adds more content to vex_schedule

--- vex_schedule.php
+++ vex_schedule.php
@@ -18,13 +18,22 @@
 			<?php include_once("site_wide.php") ?>
 			
 			<h1 class="page-header">Competition List <small>VEX 2016</small></h1>
-
+			<a href="club_schedule"><h3>For a full schedule, visit our club schedule page.</h3>
 			<div class="row">
 				<div class="col-md-6">
-					<div class="well well-lg bg-success">
-						<h2>Holy Cross Robotics Tournament</h2>
+					<div class="well well-lg">
+						<a href="http://www.robotevents.com/re-vrc-16-3753.html"><h2>Holy Cross Robotics Tournament</h2></a>
 						<h3>January 23rd</h3>
 						<h4>Holy Cross Catholic Secondary School</h4>
+						Lorem Ipsum.
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="well well-lg">
+						<a href="http://www.robotevents.com/re-vrc-16-4019.html"><h2>Rick Hansen SS Robotics Tournament</h2>
+						<h3>February 27th</h3>
+						<h4>Rich Hansen Secondary School</h4>
 						Lorem Ipsum.
 					</div>
 				</div>
