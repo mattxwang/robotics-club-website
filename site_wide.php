279e5a26ce3640dc1c2558488b3c6cc98a96692e
malsf21 <matthewang.wang@gmail.com>
2016-01-13 20:46:53 -0500

    adds another site alert

--- site_wide.php
+++ site_wide.php
@@ -3,3 +3,9 @@
 	<h4>Warning!</h4>
 	<p>You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a>.</p>
 </div>
+
+<div class="alert alert-dismissible alert-info fade in">
+	<button type="button" class="close" data-dismiss="alert">x</button>
+	<h4>Hey there!</h4>
+	<p>We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca">Matthew Wang</a> for more information.</p>
+</div>
