f3106bdcd1dbc657f848877e93328463a6a98ed3
Nicholas O'Brien <nicholas.obrien@ucc.on.ca>
2016-01-15 12:44:32 -0500

    Added Different Calendars
    schedule_switch.js is a work in progress

---
+++ js/schedule_switch.js
@@ -0,0 +1,11 @@
+/*
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 70) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}); */
+
+
+
