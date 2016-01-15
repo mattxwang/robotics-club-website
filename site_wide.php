2342d8d662927ecbbb679a33d7d853de1be1476d
malsf21 <matthewang.wang@gmail.com>
2016-01-15 08:54:12 -0500

    small glyph fix

--- site_wide.php
+++ site_wide.php
@@ -5,5 +5,5 @@
 
 <div class="alert alert-dismissible alert-info fade in">
 	<button type="button" class="close" data-dismiss="alert">x</button>
-	<p><span class="glyphicon glyphicon-alert"></span> <b>Hey there!</b> We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
+	<p><span class="glyphicon glyphicon-pushpin"></span> <b>Hey there!</b> We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
 </div>
