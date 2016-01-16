d328a1787a6465227ca9eec5ce3002a6610d4d9b
Nicholas O'Brien <nicholas.obrien@ucc.on.ca>
2016-01-16 12:11:45 -0500

    I DID IT (i think)

--- js/schedule_switch.js
+++ js/schedule_switch.js
@@ -7,12 +7,18 @@ $(window).scroll(function() {
     }
 }); */
 //Saving elements as vars
-var janStyle = document.querySelector("#January");
-var febStyle = document.querySelector("#Febuary");
-var marStyle = document.querySelector("#March");
-var aprStyle = document.querySelector("#April");
-var mayStyle = document.querySelector("#May");
-
+//Calendars
+var janStyle = document.getElementById("#January");
+var febStyle = document.getElementById("#Febuary");
+var marStyle = document.getElementById("#March");
+var aprStyle = document.getElementById("#April");
+var mayStyle = document.getElementById("#May");
+//Buttons
+var JanButton = document.getElementById("#JanButton");
+var FebButton = document.getElementById("#FebButton");
+var MarButton = document.getElementById("#MarButton");
+var AprButton = document.getElementById("#AprButton");
+var MayButton = document.getElementById("#MayButton");
 
 // dw about this
 //janStyle.Style.addClass = " ";
@@ -22,6 +28,8 @@ marStyle.Style.addClass = "hidden";
 aprStyle.Style.addClass = "hidden";
 mayStyle.Style.addClass = "hidden";
 
+//These functions change which calendars are viewable
+//for Jan
 function viewJan(){
 	janStyle.Style.removeClass = "hidden";
 	febStyle.Style.addClass = "hidden";
@@ -29,7 +37,7 @@ function viewJan(){
 	aprStyle.Style.addClass = "hidden";
 	mayStyle.Style.addClass = "hidden";
 }
-
+//for Feb
 function viewFeb(){
 	janStyle.Style.addClass = "hidden";
 	febStyle.Style.removeClass = "hidden";
@@ -37,7 +45,7 @@ function viewFeb(){
 	aprStyle.Style.addClass = "hidden";
 	mayStyle.Style.addClass = "hidden";	
 }
-
+//For March
 function viewMar(){
 	janStyle.Style.addClass = "hidden";
 	febStyle.Style.addClass = "hidden";
@@ -45,7 +53,7 @@ function viewMar(){
 	aprStyle.Style.addClass = "hidden";
 	mayStyle.Style.addClass = "hidden";
 }
-
+//For April
 function viewApr(){
 	janStyle.Style.addClass = "hidden";
 	febStyle.Style.addClass = "hidden";
@@ -53,7 +61,7 @@ function viewApr(){
 	aprStyle.Style.removeClass = "hidden";
 	mayStyle.Style.addClass = "hidden";	
 }
-
+//For May
 function viewMay(){
 	janStyle.Style.addClass = "hidden";
 	febStyle.Style.addClass = "hidden";
@@ -61,4 +69,25 @@ function viewMay(){
 	aprStyle.Style.addClass = "hidden";
 	mayStyle.Style.removeClass = "hidden";
 }
-
+//If statements
+//if button is pressed, run function
+//Shows jan
+if (JanButton.onClick == true){
+	viewJan();
+}
+//Shows feb
+if (FebButton.onClick == true) {
+	viewFeb();
+}
+//Shows mar
+if (MarButton.onClick == true) {
+	viewMar();
+}
+//Shows april
+if (AprButton.onClick == true) {
+	viewApr();
+}
+//shows may
+if (MayButton.onClick == true) {
+	viewMay();
+}
