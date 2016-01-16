debbba4c57f93b7130b5404ad9ccafa5a3fe661b
Matthew Wang <matthewang.wang@gmail.com>
2016-01-16 18:56:52 -0500

    fixes alert link

--- site_wide.php
+++ site_wide.php
@@ -1,6 +1,6 @@
 <div class="alert alert-dismissible alert-warning fade in">
 	<button type="button" class="close" data-dismiss="alert">x</button>
-	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a>.</p>
+	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website/issues" class="alert-link">repository</a>.</p>
 </div>
 
 <div class="alert alert-dismissible alert-info fade in">
