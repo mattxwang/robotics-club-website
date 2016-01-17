ef024a68fb9bd7457f1d0fd0a6029b5400837a04
Matthew Wang <matthewang.wang@gmail.com>
2016-01-16 19:15:55 -0500

    bug fix attempt

--- functions/night_mode_toggle.php
+++ functions/night_mode_toggle.php
@@ -1,14 +1,19 @@
 <?php
 	require("common.php");
+	echo "Hi";
 	if (empty($_SESSION['nightmode']){
+			echo "Hi1";
 		$_SESSION['nightmode'] = true;
 	}
 	elseif ($_SESSION['nightmode'] == false){
+			echo "Hi2";
 		$_SESSION['nightmode'] = true;
 	}
 	else{
+			echo "Hi3";
 		$_SESSION['nightmode'] = false;
 	}
+		echo "Hi";
 	header("Location: index.php"); 
 	die("Redirecting to: index.php"); 
 ?>
