0e468c3979f9f40e44899d628329799baa8a1077
Matthew Wang <matthewang.wang@gmail.com>
2016-01-17 02:22:08 -0500

    bug fixing

--- functions/toggle_nightmode.php
+++ functions/toggle_nightmode.php
@@ -47,7 +47,7 @@
 	if (empty($_SESSION['PageStyling'])){
 		$_SESSION['PageStyling'] = "whitemode";
 		if (!empty($row_info['nightmode'])){
-			if ($row_info['nightmode'] == 0 && $row_info['nightmode'] == 3){
+			if ($row_info['nightmode'] == 0 || $row_info['nightmode'] == 3){
 				$_SESSION['PageStyling'] = "whitemode";
 			}
 
