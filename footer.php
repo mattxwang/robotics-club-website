468848039893adafae4df8caff5f3584b01c023e
Matthew Wang <malsf21@users.noreply.github.com>
2016-01-19 15:14:39 -0500

    cleans up footer

--- footer.php
+++ footer.php
@@ -8,12 +8,14 @@
 			<ul class="nav navbar-nav">
 				<p class="navbar-text" style="font-size:12px;">
 				Designed by <a href="http://twitter.com/malsf21"><b>Matthew Wang</b></a>,</br>
-				developed by <a href="http://jack.antinode.ca"><b>Jack Sarick</b></a>, <a href="http://twitter.com/malsf21"><b>Matthew Wang</b></a>, and <a href="#">Nicholas O'Brien</a> <!-- I helped... -->
+				developed by <a href="http://jack.antinode.ca"><b>Jack Sarick</b></a> and <a href="http://twitter.com/malsf21"><b>Matthew Wang</b>.</a>
 				</p>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
 				<p class="navbar-text" style="font-size:8px">
 				This website utilises <a href="http://getbootstrap.com">the bootstrap framework</a>.
+				</br>
+				We also use <a href="https://en.wikipedia.org/wiki/HTTP_cookie">cookies</a> to make the user experience awesome.
 				</br>
 				Source code is available <a href="https://github.com/malsf21/robotics-club-website">here</a>.
 				</p>
