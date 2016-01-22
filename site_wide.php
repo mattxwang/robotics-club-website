e32ef0b226fe8d0a5405642f8b7ac953a5d037c1
Matthew Wang <matthewang.wang@gmail.com>
2016-01-22 16:42:32 -0500

    more alert bugfixing

--- site_wide.php
+++ site_wide.php
@@ -1,30 +1,31 @@
 <?php
+	require("functions/common.php");
+	if (empty($_SESSION['alerts'])){
+		$_SESSION['alerts']['alert1'] == False;
+		$_SESSION['alerts']['alert2'] == False;
+	}
+?>
+<?php
 	function viewedAlert(){
 
 		if (isset($_GET['viewedAlert'])) {
 	    $alert_value = $_GET['viewedAlert'];
-		}
-		if (empty($_SESSION['alerts'])){
-			$_SESSION['alerts']['alert1'] == True;
-			$_SESSION['alerts']['alert2'] == True;
-		}
-		else{
 			if ($alert_value == 1){
-				$_SESSION['alerts']['alert1'] == False;
+				$_SESSION['alerts']['alert1'] == True;
 			}
 			elseif ($alert_value == 2){
-				$_SESSION['alerts']['alert2'] == False;
+				$_SESSION['alerts']['alert2'] == True;
 			}
 		}
 	}
 	viewedAlert();
 ?>
-<?php if ($_SESSION['alerts']['alert1'] == True) { ?>
+<?php if ($_SESSION['alerts']['alert1'] == False) { ?>
 <div class="alert alert-dismissible alert-warning fade in">
 	<button type="button" class="close" data-dismiss="alert" href="?viewedAlert=1">x</button>
 	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website/issues" class="alert-link">repository</a>.</p>
 </div>
-<?php } if ($_SESSION['alerts']['alert2'] == True) { ?>
+<?php } if ($_SESSION['alerts']['alert2'] == False) { ?>
 
 <div class="alert alert-dismissible alert-info fade in">
 	<button type="button" class="close" data-dismiss="alert" href="?viewedAlert=2">x</button>
