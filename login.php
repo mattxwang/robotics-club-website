08112c08efbcae2337386b9bd4bdd85d260752d2
Matthew Wang <matthewang.wang@gmail.com>
2016-01-22 09:43:10 -0500

    modularises login function

--- login.php
+++ login.php
@@ -1,4 +1,75 @@
 <?php include_once("functions/import_info.php") ?>
+<?php
+	function login(){
+		if (isset($_GET['login'])){
+			require("functions/common.php");
+
+			$submitted_email = '';
+			$correction = 'none';
+
+			if(!empty($_POST))
+			{
+				$query = "
+					SELECT *
+					FROM users
+					WHERE
+						email = :email";
+
+				$query_params = array(
+					':email' => $_POST['email']
+				);
+
+				try
+				{
+					$stmt = $db->prepare($query);
+					$result = $stmt->execute($query_params);
+				}
+				catch(PDOException $ex)
+				{
+					die("Failed to run query: " . $ex->getMessage());
+				}
+
+				$login_ok = false;
+
+				$row = $stmt->fetch();
+				if($row)
+				{
+					$check_password = hash('sha256', $_POST['password'] . $row['salt']);
+					for($round = 0; $round < 65536; $round++)
+					{
+						$check_password = hash('sha256', $check_password . $row['salt']);
+					}
+
+					if($check_password === $row['password'])
+					{
+						$login_ok = true;
+					}
+				}
+
+				if($login_ok)
+				{
+					unset($row['salt']);
+					unset($row['password']);
+
+					$_SESSION['user'] = $row;
+
+					header("Location: user_home.php");
+					die("Redirecting to: user_home.php");
+				}
+				else
+				{
+					$correction = 'block';
+
+					$submitted_email = htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');
+
+					header("Location: login.php");
+					die("Redirecting to: login.php");
+				}
+			}
+		}
+	}
+	login();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -17,7 +88,7 @@
 			<?php include_once("site_wide.php") ?>
 			<h1 class="page-header">Login</h1>
 			<h3>Don't have an account? <b><a href="register.php">Register</a></b>.</h3>
-			<form class="form-signin" action="functions/login.php" method="post">
+			<form class="form-signin" action="?login" method="post">
 				<div class = "row">
 					<div class = "col-md-12">
 						<input type="email" id="email" class="form-control" placeholder="john.smith@ucc.on.ca" name="email" required="" autofocus="">
