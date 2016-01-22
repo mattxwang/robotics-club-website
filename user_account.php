3dcee87ad9cfb71c92eb0972402cdeb07969724d
Matthew Wang <matthewang.wang@gmail.com>
2016-01-22 16:08:55 -0500

    modularises changePassword

--- user_account.php
+++ user_account.php
@@ -57,6 +57,63 @@
 		}
 	}
 	accountNightmode();
+
+	function changePassword(){
+		require("functions/common.php");
+		require("functions/import_info.php");
+		if (isset($_GET['changePassword'])) {
+			if(!empty($_POST)) {
+				if(empty($_POST['password']) || empty($_POST['password_new_1']) || empty($_POST['password_new_2'])) {
+					die("You missed a field");
+				}
+
+				if($_POST['password_new_1'] != $_POST['password_new_2']) {
+					die("New Password Mismatch");
+				}
+
+				$salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
+
+				$password = hash('sha256', $_POST['password_new_1'] . $salt);
+
+				for($round = 0; $round < 65536; $round++) {
+					$password = hash('sha256', $password . $salt);
+				}
+
+				$email = $_SESSION['user']['email'];
+				$admin = $_SESSION['user']['admin'];
+
+				$query = "
+				REPLACE INTO users (
+					email,
+					password,
+					salt,
+					admin
+				) VALUES (
+					'$email',
+					'$password',
+					'$salt',
+					'$admin'
+				);";
+
+				try {
+					$stmt = $db->prepare($query);
+					$result = $stmt->execute($query_params);
+
+					header("Location: ".$_SERVER['SCRIPT_NAME']);
+
+					die();
+				}
+				catch(PDOException $ex) {
+					die("Failed to run query: " . $ex->getMessage());
+				}
+
+			}
+
+			else {
+				echo "No password was submitted.";
+			}
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -84,7 +141,7 @@
 						</div>
 						<div class="modal-body">
 							<p>
-							<form action="functions/update_users.php" method="post">
+							<form action="?changePassword" method="post">
 								<h4>Old Password</h4>
 								<label for="inputPassword" class="sr-only">Old Password</label>
 								<input type="password" id="password" name="password" class="form-control" placeholder="Old Password" required="" autofocus="">
