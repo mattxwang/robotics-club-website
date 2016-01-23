cc4b1b299da34057ee9daf8ae8a6f31bd2d3999a
Matthew Wang <matthewang.wang@gmail.com>
2016-01-23 18:20:31 -0500

    Reformats footer

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 <!-- Footer -->
 <nav class="navbar navbar-default navbar-fixed-bottom navbar-footer hidden-xs top-nav-collapse">
 	<div class="container-fluid navbar-footer">
-		<div class="navbar-header">
-			
-		</div>
 		<div class="collapse navbar-collapse" id="footer-navbar">
 			<ul class="nav navbar-nav">
 				<p class="navbar-text" style="font-size:12px;">
@@ -15,9 +12,9 @@
 				<p class="navbar-text" style="font-size:8px">
 				This website utilises <a href="http://getbootstrap.com">the bootstrap framework</a>.
 				</br>
-				We also use <a href="https://en.wikipedia.org/wiki/HTTP_cookie">cookies</a> to make the user experience awesome.
+				We use <a href="https://en.wikipedia.org/wiki/HTTP_cookie">cookies</a> and Google Analytics to make your user experience awesome.
 				</br>
-				Source code is available <a href="https://github.com/malsf21/robotics-club-website">here</a>, with team/resource list <a href="https://github.com/malsf21/robotics-club-website/blob/master/CREDITS.md"> here</a>.
+				The source code is available <a href="https://github.com/malsf21/robotics-club-website">here</a>, with a team/resource list <a href="https://github.com/malsf21/robotics-club-website/blob/master/CREDITS.md"> here</a>.
 				</p>
 			</ul>
 		</div>
