7e73a8ac6cb78c309ebdf9599922fa9b2dfb80c3
Matthew Wang <matthewang.wang@gmail.com>
2016-01-23 11:22:54 -0500

    typecasts date variables

--- user_home.php
+++ user_home.php
@@ -8,7 +8,7 @@
 ?>
 <?php
 $timestamp = getdate();
-$date = $timestamp['year'] .  "-" . $timestamp['month'] . "-" . $timestamp['mday'];
+$date = (string) $timestamp['year'] .  "-" . (string) $timestamp['month'] . "-" . (string) $timestamp['mday'];
 function checkIn(){
 	if (isset($_GET['checkIn'])) {
 		require("functions/common.php");
