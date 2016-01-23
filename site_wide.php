c3e03c4141d6b208164a04baa8748a3f66e0a322
Matthew Wang <matthewang.wang@gmail.com>
2016-01-23 13:00:38 -0500

    reverts alerts to their previous state for now

--- site_wide.php
+++ site_wide.php
@@ -1,4 +1,5 @@
 <?php
+	/*
 	require("functions/common.php");
 	if (empty($_SESSION['alerts'])){
 		$_SESSION['alerts'] = ['alert1','alert2'];
@@ -24,17 +25,18 @@
 		}
 	}
 	viewedAlert();
+	*/
 ?>
-<?php if ($_SESSION['alerts']['alert1'] == false) { ?>
+<?php /*if ($_SESSION['alerts']['alert1'] == false) { */?>
 <div class="alert alert-dismissible alert-warning fade in">
 	<button type="button" class="close" data-dismiss="alert" href="?viewedAlert=1">x</button>
 	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. It might have some issues, glaring bugs, or utter failures. If you experience any of those, please report them to the <a href="https://github.com/malsf21/robotics-club-website/issues" class="alert-link">repository</a>.</p>
 </div>
-<?php } if ($_SESSION['alerts']['alert2'] == false) { ?>
+<?php /*} if ($_SESSION['alerts']['alert2'] == false) { */?>
 
 <div class="alert alert-dismissible alert-info fade in">
 	<button type="button" class="close" data-dismiss="alert" href="?viewedAlert=2">x</button>
 	<p><span class="glyphicon glyphicon-pushpin"></span> <b>Hey there!</b> We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
 </div>
 
-<?php } ?>
+<?php /*}*/ ?>
