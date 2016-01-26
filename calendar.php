257d68653a294f980d3282a33a649667e07b7cb4
Nicholas O'Brien <nicholas.obrien@ucc.on.ca>
2016-01-26 14:05:42 -0500

    Calendar update numero uno

---
+++ calendar.php
@@ -0,0 +1,76 @@
+<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td class="bg-success" style="color:white;">23
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="VEX Competition @ Holy Cross" data-content="Our competition on the 23rd is our first competition this year! We'll compete in a round-robin, and hopefully the playoffs, of the VEX Nothing But Net competition at Holy Cross. To learn more, contact <a href='mailto:matthew.wang@ucc.on.ca'>Matthew Wang</a>." data-html="true" style="color:inherit;"><h6>VEX Competition @ Holy Cross</h6></a>
+						</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>
--- js/schedule_switch.js
+++ js/schedule_switch.js
@@ -7,11 +7,7 @@ $(window).scroll(function() {
     }
 }); */
 //Calendars
-var JanCalendar = document.getElementById("January");
-var FebCalendar = document.getElementById("Febuary");
-var MarCalendar = document.getElementById("March");
-var AprCalendar = document.getElementById("April");
-var MayCalendar = document.getElementById("May");
+var calendar = document.getElementById("Calendar");
 //Buttons
 var JanButton = document.getElementById("JanButton");
 var FebButton = document.getElementById("FebButton");
@@ -19,49 +15,395 @@ var MarButton = document.getElementById("MarButton");
 var AprButton = document.getElementById("AprButton");
 var MayButton = document.getElementById("MayButton");
 
-//Start with all but Jan hidden
-FebCalendar.addClass("hidden");
-MarCalendar.addClass("hidden");
-AprCalendar.addClass("hidden");
-MayCalendar.addClass("hidden");
 
 function JanClicked(){
-	JanCalendar.removeClass("hidden");
-	FebCalendar.addClass("hidden");
-	MarCalendar.addClass("hidden");
-	AprCalendar.addClass("hidden");
-	MayCalendar.addClass("hidden");
+	calendar.document.write(<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td class="bg-success" style="color:white;">23
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="VEX Competition @ Holy Cross" data-content="Our competition on the 23rd is our first competition this year! We'll compete in a round-robin, and hopefully the playoffs, of the VEX Nothing But Net competition at Holy Cross. To learn more, contact <a href='mailto:matthew.wang@ucc.on.ca'>Matthew Wang</a>." data-html="true" style="color:inherit;"><h6>VEX Competition @ Holy Cross</h6></a>
+						</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>)
 }
 
 function FebClicked(){
-	JanCalendar.addClass("hidden");
-	FebCalendar.removeClass("hidden");
-	MarCalendar.addClass("hidden");
-	AprCalendar.addClass("hidden");
-	MayCalendar.addClass("hidden");
+	calendar.document.write(<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td>23</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>)
+	
 }
 
 function MarClicked(){
-	JanCalendar.addClass("hidden");
-	FebCalendar.addClass("hidden");
-	MarCalendar.removeClass("hidden");
-	AprCalendar.addClass("hidden");
-	MayCalendar.addClass("hidden");
+	calendar.document.write(<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td>23</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>)
+	
 }
 
 function AprClicked(){
-	JanCalendar.addClass("hidden");
-	FebCalendar.addClass("hidden");
-	MarCalendar.addClass("hidden");
-	AprCalendar.removeClass("hidden");
-	MayCalendar.addClass("hidden");
+	calendar.document.write(<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td>23</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>)
+	
 }
 
 function MayClicked(){
-	JanCalendar.addClass("hidden");
-	FebCalendar.addClass("hidden");
-	MarCalendar.addClass("hidden");
-	AprCalendar.addClass("hidden");
-	MayCalendar.removeClass("hidden");
+	calendar.document.write(<div id="Calendar" class = "table-responsive ">
+				<table class = "table">
+					<tr>
+						<td>Sunday</td>
+						<td>Monday</td>
+						<td>Tuesday</td>
+						<td>Wednesday</td>
+						<td>Thursday</td>
+						<td>Friday</td>
+						<td>Saturday</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td class="bg-primary" style="color:white;">7
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+					<tr> 
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+						<td class="bg-primary" style="color:white;">14
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>15</td>
+						<td>16</td>
+					</tr>
+					<tr> 
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+						<td class="bg-primary" style="color:white;">21
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>22</td>
+						<td>23</td>
+					</tr>
+					<tr> 
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+						<td class="bg-primary" style="color:white;">28
+							</br><a tabindex="0" role="button" data-trigger="focus" data-toggle="popover" data-container="body" data-placement="auto top" title="Horizons Robotics" data-content="<a href='horizons_about.php'>Horizons Robotics</a> in room 304 gives club members an opportunity to give back to the community, by teaching younger students about Robotics. In order to participate, members must have attended a Horizons training session first." data-html="true" style="color:inherit;"><h6>Horizons Robotics</h6></a>
+						</td>
+						<td>29</td>
+						<td>30</td>
+					</tr>
+					<tr>
+						<td>31</td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+				</table>
+			</div>)
+	
 }
 
