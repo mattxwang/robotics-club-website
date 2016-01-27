384f0d2ec214f1a4dc625407c932cf1534d3dd0a
Matthew Wang <matthewang.wang@gmail.com>
2016-01-27 14:52:37 -0500

    Fills out carousel for index

--- index.php
+++ index.php
@@ -65,6 +65,39 @@
 					</div>
 				</div>
 			</div>
+			<div class="jumbotron">
+				<div id="student-spotlight-carousel" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner" role="listbox">
+				    <div class="item active">
+							<div class="row">
+								<div class="col-md-8">
+									<h1 class="page-header">Student Spotlight</h1>
+									<h3>Matthew Wang</h3>
+								</div>
+								<div class="col-md-4">
+									<div class="thumbnail">
+										<img src="img/placeholder.png" height="256px"></img>
+									</div>
+								</div>
+							</div>
+				    </div>
+				    <div class="item">
+				      <img src="..." alt="...">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    ...
+				  </div>
+					<a class="left carousel-control" href="#student-spotlight-carousel" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#student-spotlight-carousel" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
 		</div>
 
 		<script src="js/jquery.js"></script>
