a1ab0bf158647acbc4709468790bb430bdd00433
Matthew Wang <matthewang.wang@gmail.com>
2016-01-28 10:57:22 -0500

    creates working bulletin add code, changes a bit of bulletin styling

--- user_admin.php
+++ user_admin.php
@@ -26,6 +26,9 @@
 
 	$code_info = $stmt->fetch();
 	$today_code = $code_info['date'];
+	$timestamp = getdate();
+	$date = (string) $timestamp['year'] .  "-" . (string) $timestamp['mon'] . "-" . (string) $timestamp['mday'];
+	$day = (string) $timestamp['weekday'] . " " . (string) $timestamp['month'] .  " " . (string) $timestamp['mday'] . " " . (string) $timestamp['year'];
 	function changeCode(){
 		if (isset($_GET['changeCode'])) {
 			require("functions/common.php");
@@ -60,6 +63,52 @@
 		}
 	}
 	changeCode();
+	function newAnnouncement(){
+		require("functions/common.php");
+		require("functions/import_info.php");
+		if (isset($_GET['newAnnouncement'])) {
+
+			$_POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+			$first_name = $row_info['first_name'];
+			$last_name = $row_info['last_name'];
+			$title = $_POST['title'];
+			$tag = $_POST['tag'];
+			$content = $_POST['content'];
+			$expire = $timestamp; //+ 2weeks of Unix time converted to YYYY MM DD
+
+			$query = "
+		 	INSERT INTO bulletin (
+				first_name,
+				last_name,
+				title,
+				tag,
+				content,
+				expire
+			) VALUES (
+				'$first_name',
+				'$last_name',
+				'$title',
+				'$tag',
+				'$content',
+				'$expire'
+			);";
+
+			try {
+				$stmt = $db->prepare($query);
+				$stmt->execute();
+
+				header("Location: ".$_SERVER['SCRIPT_NAME']);
+			}
+
+			catch(PDOException $ex)
+			{
+				die("Failed to run query: " . $ex->getMessage());
+				header("Location: ".$_SERVER['SCRIPT_NAME']);
+			}
+		}
+	}
+	newAnnouncement();
 ?>
 <!DOCTYPE html>
 <html>
@@ -77,6 +126,51 @@
 		<?php include_once("footer.php")  ?>
 		<div class="container">
 			<?php include_once("site_wide.php") ?>
+
+			<!-- New Bulletin Announcement -->
+			<div class="modal fade" tabindex="-1" role="dialog" id="newAnnouncement">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
+							<h4 class="modal-title">New Announcement</h4>
+						</div>
+						<div class="modal-body">
+							<p>
+							<form action="?newAnnouncement" method="post">
+								<h2>New Bulletin Announcement</h2>
+								<p>
+									Bulletin announcements are displayed on the <a href="user_home.php">bulletin board on the user homepage</a>. They stay active for two weeks, and then disappear.
+								</p>
+								<div class="row">
+									<div class="col-md-8">
+										<h4>Announcement Title</h4>
+										<input type="text" id="title" name="title" class="form-control" placeholder="Bulletin Announcement!" required="">
+									</div>
+									<div class="col-md-4">
+										<h4>Announcement Tag</h4>
+										<select class="form-control" id="tag" name="tag" required="">
+											<option value="default"><span class="label label-default">News</span> Tag</option>
+											<option value="primary"><span class="label label-primary">Club Events</span> Tag</option>
+											<option value="warning"><span class="label label-warning">Horizons</span> Tag</option>
+											<option value="danger"><span class="label label-danger">Administrative</span> Tag</option>
+										</select>
+									</div>
+								</div>
+								<h4>Announcement Content</h4>
+								<textarea class="form-control" rows="4" id="content" name="content" placeholder="Hey guys, we have an announcement for you..." required="">
+
+								</textarea>
+						</div>
+						<div class="modal-footer">
+								<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+								<button type="submit" class="btn btn-success" id="submitbutton" value="Login">Submit Bulletin</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<div class="row">
 				<div class="col-md-4">
 					<div class="panel panel-primary">
@@ -100,6 +194,7 @@
 				</div>
 				<div class="col-md-8">
 					<div class="well well-lg">
+						<h2>Today is <b><?php echo $day; ?></b></h2>
 						<form class="form-signin" action="?changeCode" method="post">
 							<div class="row">
 								<div class="col-sm-4">
