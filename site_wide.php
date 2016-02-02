04d2a044ee9a543836a2f15d172841533faf6b52
Matthew Wang <matthewang.wang@gmail.com>
2016-02-02 14:03:51 -0500

    tests out alert cookies

--- site_wide.php
+++ site_wide.php
@@ -1,3 +1,27 @@
+<!-- StackOverflow http://stackoverflow.com/questions/13408244/using-bootstrap-have-alert-box-remember-close-action -->
+<script src="js/js.cookie.min.js"></script>
+<script>
+jQuery(function( $ ){
+
+	if( $.cookie('alert-cookie-1') === 'closed' ){
+      $('.alert-1').hide();
+  }
+
+	if( $.cookie('alert-cookie-2') === 'closed' ){
+      $('.alert-2').hide();
+  }
+
+	$('.close-1').click(function( e ){
+		e.preventDefault();
+		$.cookie('alert-cookie-1', 'closed', { path: '/' });
+	});
+
+	$('.close-2').click(function( e ){
+		e.preventDefault();
+		$.cookie('alert-cookie-2', 'closed', { path: '/' });
+	});
+});
+</script>
 <?php
 	/*
 	require("functions/common.php");
@@ -27,15 +51,22 @@
 	viewedAlert();
 	*/
 ?>
+<?php
+	/*
+	for ($i = 1; $i <= 10; $i++) {
+		echo "<div class='alert alert-dismissible fade in ". $alert_type . "'><button type='button' class='close' data-dismiss='alert'>x</button><p><span class='glyphicon " . $alert_glyph . "'></span> " . $alert_content . "</p></div>";
+	}
+	*/
+?>
 <?php /*if ($_SESSION['alerts']['alert1'] == false) { */?>
-<div class="alert alert-dismissible alert-warning fade in">
-	<button type="button" class="close" data-dismiss="alert">x</button>
+<div class="alert alert-dismissible alert-warning fade in alert-1">
+	<button type="button" class="close close-1" data-dismiss="alert">x</button>
 	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. Please report any issues, glaring bugs, or utter failures, to the <a href="https://github.com/malsf21/robotics-club-website/issues" class="alert-link">repository</a>.</p>
 </div>
 <?php /*} if ($_SESSION['alerts']['alert2'] == false) { */?>
 
-<div class="alert alert-dismissible alert-info fade in">
-	<button type="button" class="close" data-dismiss="alert">x</button>
+<div class="alert alert-dismissible alert-info fade in alert-1">
+	<button type="button" class="close close-2" data-dismiss="alert">x</button>
 	<p><span class="glyphicon glyphicon-pushpin"></span> <b>Hey there!</b> We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
 </div>
 
