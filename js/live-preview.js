5f0b4d8a064d7836c1b9bc1ec1c56355744af62e
Matthew Wang <matthewang.wang@gmail.com>
2016-02-01 23:10:59 -0500

    fixes id reference

--- js/live-preview.js
+++ js/live-preview.js
@@ -2,10 +2,10 @@
 $('.live-preview').change(function(){
   var $this = $(this);
   $('.' + $this.attr('id') + '').html($this.val());
-  document.getElementById('alert-nest').innerHTML= 'id';
+  document.getElementById('alert-nest').innerHTML= $this.attr('id');
 });
 $('.live-preview').keyup(function(){
   var $this = $(this);
   $('.' + $this.attr('id') + '').html($this.val());
-  document.getElementById('alert-nest').innerHTML= 'id';
+  document.getElementById('alert-nest').innerHTML= $this.attr('id');
 });
