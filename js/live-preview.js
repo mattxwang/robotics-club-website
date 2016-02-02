610cbae6012fd7eda04dc7ba52855a5f82de5c41
Matthew Wang <matthewang.wang@gmail.com>
2016-02-01 23:12:23 -0500

    should fix this reference once and for all

--- js/live-preview.js
+++ js/live-preview.js
@@ -2,10 +2,10 @@
 $('.live-preview').change(function(){
   var $this = $(this);
   $('.' + $this.attr('id') + '').html($this.val());
-  document.getElementById('alert-nest').innerHTML= $this.attr('id');
+  document.getElementById('alert-nest').innerHTML= html($this.val());
 });
 $('.live-preview').keyup(function(){
   var $this = $(this);
   $('.' + $this.attr('id') + '').html($this.val());
-  document.getElementById('alert-nest').innerHTML= $this.attr('id');
+  document.getElementById('alert-nest').innerHTML= html($this.val());
 });
