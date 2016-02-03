87900e68501fb29e9b0c32a6bb2c30e86812e58f
Matthew Wang <matthewang.wang@gmail.com>
2016-02-03 18:54:51 -0500

    removes uneeded php

--- site_wide.php
+++ site_wide.php
@@ -21,52 +21,11 @@ jQuery(function( $ ){
 	});
 });
 </script>
-<?php
-	/*
-	require("functions/common.php");
-	if (empty($_SESSION['alerts'])){
-		$_SESSION['alerts'] = ['alert1','alert2'];
-	}
-	if (empty($_SESSION['alerts']['alerts1'])){
-		$_SESSION['alerts']['alert1'] == false;
-	}
-	if (empty($_SESSION['alerts']['alerts2'])){
-		$_SESSION['alerts']['alert2'] == false;
-	}
-?>
-<?php
-	function viewedAlert(){
-		if (isset($_GET['viewedAlert'])) {
-	    $alert_value = $_GET['viewedAlert'];
-			if ($alert_value == 1){
-				$_SESSION['alerts']['alert1'] == true;
-			}
-			elseif ($alert_value == 2){
-				$_SESSION['alerts']['alert2'] == true;
-			}
-			header("Location: ".$_SERVER['SCRIPT_NAME']);
-		}
-	}
-	viewedAlert();
-	*/
-?>
-<?php
-	/*
-	for ($i = 1; $i <= 10; $i++) {
-		echo "<div class='alert alert-dismissible fade in ". $alert_type . "'><button type='button' class='close' data-dismiss='alert'>x</button><p><span class='glyphicon " . $alert_glyph . "'></span> " . $alert_content . "</p></div>";
-	}
-	*/
-?>
-<?php /*if ($_SESSION['alerts']['alert1'] == false) { */?>
 <div class="alert alert-dismissible alert-warning fade in alert-1">
 	<button type="button" class="close close-1" data-dismiss="alert">x</button>
 	<p><span class="glyphicon glyphicon-alert"></span> <b>Warning!</b> You're visiting a beta version of this site. Please report any issues, glaring bugs, or utter failures, to the <a href="https://github.com/malsf21/robotics-club-website/issues" class="alert-link">repository</a>.</p>
 </div>
-<?php /*} if ($_SESSION['alerts']['alert2'] == false) { */?>
-
 <div class="alert alert-dismissible alert-info fade in alert-2">
 	<button type="button" class="close close-2" data-dismiss="alert">x</button>
 	<p><span class="glyphicon glyphicon-pushpin"></span> <b>Hey there!</b> We're looking for students to help us out with this website! Fork our <a href="https://github.com/malsf21/robotics-club-website" class="alert-link">repository</a> or contact <a href="mailto:matthew.wang@ucc.on.ca" class="alert-link">Matthew Wang</a> for more information.</p>
 </div>
-
-<?php /*}*/ ?>
