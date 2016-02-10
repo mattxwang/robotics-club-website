2de52990ad2d6b492b9ae1c533ac555935d6bbc8
Jack Sarick <jacksarick@gmail.com>
2016-02-10 13:01:05 -0500

    vardump session to console

--- login.php
+++ login.php
@@ -73,6 +73,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<script type="text/javascript">
+			console.log(<?php var_dump($_SESSION['user']; ?>);
+		</script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<meta name="description" content="UCC Robotics">
